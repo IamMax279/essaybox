6d7e69eb339dbf31d16adaab66994ec394468741
IamMax279 <maksssiula@gmail.com>
2025-05-26 19:01:57 +0200

    initialize main.ts with expressjs

---
+++ server/src/main.ts
@@ -0,0 +1,10 @@
+import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config()
+const app = express()
+
+app.use(express.json())
+
+const PORT = process.env.PORT || 3001
+app.listen(PORT)
