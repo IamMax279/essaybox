a95e3ec3de17c4a1e50dde74b5071143718f1804
IamMax279 <maksssiula@gmail.com>
2025-05-31 14:26:37 +0200

    add loading state indicator to the BigButton component

--- client/src/components/BigButton.tsx
+++ client/src/components/BigButton.tsx
@@ -1,19 +1,33 @@
+import { ThreeDot } from "react-loading-indicators"
+
 interface Props {
     width: string
     onPress?: () => void
     text: string
     className?: string
+    loading?: boolean
+    type?: 'submit' | 'reset' | 'button' | undefined
 }
 
-export default function BigButton({ width, onPress, text, className }: Props) {
+export default function BigButton({ width, onPress, text, className, loading, type }: Props) {
     return (
         <button className={`flex justify-center items-center bg-bigbutton py-2 rounded-lg
         cursor-pointer hover:brightness-110 transition duration-200
         ${width}
-        ${className ? className : ""}`} onClick={onPress}>
+        ${className ? className : ""}`}
+        type={type ? type : undefined}
+        onClick={onPress}
+        disabled={loading}
+        >
+            {!loading ?
             <p className="font-heming text-lg text-white">
                 {text}
             </p>
+            :
+            <div className="py-[1px]">
+                <ThreeDot color="#fff" size="small"/>
+            </div>
+            }
         </button>
     )
 }
