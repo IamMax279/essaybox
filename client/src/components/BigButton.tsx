d1af0b80d94bd245108b4a571a4d8eea8a7fc115
IamMax279 <maksssiula@gmail.com>
2025-06-02 20:36:46 +0200

    add optional shadow to the BigButton component

--- client/src/components/BigButton.tsx
+++ client/src/components/BigButton.tsx
@@ -6,13 +6,15 @@ interface Props {
     text: string
     className?: string
     loading?: boolean
+    shadow?: boolean
     type?: 'submit' | 'reset' | 'button' | undefined
 }
 
-export default function BigButton({ width, onPress, text, className, loading, type }: Props) {
+export default function BigButton({ width, onPress, text, className, loading, type, shadow }: Props) {
     return (
         <button className={`flex justify-center items-center bg-bigbutton py-2 rounded-lg
         cursor-pointer hover:brightness-110 transition duration-200
+        ${shadow ? "shadow-[0_2px_16px_0_#1157a6]" : ""}
         ${width}
         ${className ? className : ""}`}
         type={type ? type : undefined}
