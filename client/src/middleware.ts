20c173cf02635cc90c8db9164b5aae777c9f381a
IamMax279 <maksssiula@gmail.com>
2025-06-03 20:23:06 +0200

    modify middleware so that it includes the /chat/nowy page

--- client/src/middleware.ts
+++ client/src/middleware.ts
@@ -2,18 +2,23 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
 export function middleware(request: NextRequest) {
-    const cookie = request.cookies.get('connect.sid')
-    const { pathname } = request.nextUrl
+  const cookie = request.cookies.get('connect.sid')
+  const { pathname } = request.nextUrl
 
-    const authPages = ["/sign-up", "/sign-in", "/verify-email", "/"]
+  const authPages = ["/sign-up", "/sign-in", "/verify-email"]
+  const authorized = ["/chat/nowy"]
 
-    if (cookie && authPages.some((page) => pathname.startsWith(page))) {
-        return NextResponse.redirect(new URL("/", request.url)) // request.url is the base url
-    }
+  if (cookie && authPages.some((page) => (pathname.startsWith(page) || pathname === '/'))) {
+      return NextResponse.redirect(new URL("/chat/nowy", request.url)) // request.url is the base url
+  }
 
-    return NextResponse.next()
+  if (!cookie && authorized.some((page) => pathname.startsWith(page))) {
+    return NextResponse.redirect(new URL("/sign-in", request.url))
+  }
+
+  return NextResponse.next()
 }
 
 export const config = {
-  matcher: ["/sign-in", "/sign-up", "/verify-email", "/"],
+  matcher: ["/", "/sign-up", "/verify-email", "/sign-in", "/chat/nowy"],
 };
