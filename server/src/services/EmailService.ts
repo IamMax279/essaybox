f4b643170549ede605f4f4931e2d5593b4055b21
IamMax279 <maksssiula@gmail.com>
2025-06-04 22:40:43 +0200

    add a function for sending password reset emails

--- server/src/services/EmailService.ts
+++ server/src/services/EmailService.ts
@@ -1,5 +1,7 @@
 import { Resend } from "resend";
 import { RestResponse } from "../types/ResponseTypes";
+import { randomBytes } from "crypto";
+import prisma from "../../prisma/PrismaClient";
 
 const resend = new Resend(process.env.RESEND_API_KEY)
 
@@ -21,6 +23,40 @@ export class EmailService {
         } catch (error) {
             console.log("Error sending email:", error)
             throw new Error(`Coś poszło nie tak. Spróbuj ponownie później.`)
+        }
+    }
+
+    static async sendPasswordChangeEmail(receiver: string): Promise<RestResponse> {
+        const token = randomBytes(32).toString("hex")
+        const tokenExpiry = new Date(Date.now() + 1000 * 60 * 60)
+
+        const user = await prisma.user.findUnique({ where: { email: receiver } })
+        if (!user) {
+            throw new Error("Konto powiązane z tym adresem e-mail nie istnieje")
+        }
+        if (user.provider === "google") {
+            throw new Error("To konto zostało utworzone przez Google. Zaloguj się używając Google.")
+        }
+        if (user.passwordToken && user.passwordTokenExpiry && user.passwordTokenExpiry > new Date()) {
+            throw new Error("Link do zmiany hasła został już wysłany. Sprawdź swoją skrzynkę e-mail.")
+        }
+
+        await prisma.user.update({
+            where: { email: receiver },
+            data: { passwordToken: token, passwordTokenExpiry: tokenExpiry }
+        })
+
+        await resend.emails.send({
+        from: 'noreply@essaybox.pl',
+        to: receiver,
+        subject: 'Zmiana hasła Twojego konta EssayBox',
+        html: `<p>Kliknij, aby zmienić hasło do swojego konta:</p>
+        <a href="${process.env.CLIENT_URL}/reset-password?token=${token}">Zmień hasło</a>`,
+        })
+
+        return {
+            success: true,
+            message: "Email sent successfully"
         }
     }
 }
