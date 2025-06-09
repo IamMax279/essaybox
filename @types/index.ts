0b1c83b5563c1b5e9499cf3dbd8a8af313b6b6c6
IamMax279 <maksssiula@gmail.com>
2025-06-09 21:20:29 +0200

    move types into a shared folder

---
+++ @types/index.ts
@@ -0,0 +1,28 @@
+export interface RestResponse {
+    success: boolean,
+    message: string
+}
+
+export interface AIResponse extends RestResponse {
+    essay: string
+}
+
+export interface ParagraphData {
+    id: string
+    argument: string
+    customArgument: string
+    przyklad: string
+    customPrzyklad: string
+    kontekst: string
+    customKontekst: string
+}
+
+export interface GenerationParams {
+    topic: string
+    parasAmount: number
+    parasData: ParagraphData[]
+    thesisType: 'ai_generated' | 'wlasna'
+    thesis: string
+    lowerBound: number
+    upperBound: number
+}
--- server/src/types/ResponseTypes.ts
+++
@@ -1,4 +0,0 @@
-export interface RestResponse {
-    success: boolean,
-    message: string
-}
