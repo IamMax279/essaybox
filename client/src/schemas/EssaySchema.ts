e35ed4c3dc7c60f1211b6bbf46a7d18e43a493dd
IamMax279 <maksssiula@gmail.com>
2025-06-09 21:24:20 +0200

    remove the essay schema as it's not needed anymore (went back to local state, no need to use formik here)

--- client/src/schemas/EssaySchema.ts
+++
@@ -1,9 +0,0 @@
-import * as yup from "yup"
-
-const topic = yup.string()
-.min(8, "Zbyt krótki temat")
-.max(160, "Zbyt długi temat")
-
-export const essaySchema = yup.object().shape({
-    topic
-})
