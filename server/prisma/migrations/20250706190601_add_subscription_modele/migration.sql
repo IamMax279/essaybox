9096764451e6adbcc92866c3f40a944f6eb999d2
IamMax279 <maksssiula@gmail.com>
2025-07-06 23:04:35 +0200

    add subscription model and add stripeCustomerId field to user model

---
+++ server/prisma/migrations/20250706190601_add_subscription_modele/migration.sql
@@ -0,0 +1,21 @@
+-- CreateTable
+CREATE TABLE "subscriptions" (
+    "id" BIGSERIAL NOT NULL,
+    "stripeSubscriptionId" TEXT NOT NULL,
+    "status" TEXT NOT NULL,
+    "currentPeriodEnd" TIMESTAMP(3) NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "userId" BIGINT NOT NULL,
+
+    CONSTRAINT "subscriptions_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "subscriptions_stripeSubscriptionId_key" ON "subscriptions"("stripeSubscriptionId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "subscriptions_userId_key" ON "subscriptions"("userId");
+
+-- AddForeignKey
+ALTER TABLE "subscriptions" ADD CONSTRAINT "subscriptions_userId_fkey" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
