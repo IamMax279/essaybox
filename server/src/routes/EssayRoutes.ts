7a953fd4778596e53ddee9988be992385552bd24
IamMax279 <maksssiula@gmail.com>
2025-07-31 14:47:55 +0200

    integrate convertapi to convert essay txt to pdf

--- server/src/routes/EssayRoutes.ts
+++ server/src/routes/EssayRoutes.ts
@@ -1,5 +1,11 @@
 import { Request, Response, Router } from "express";
 import { EssayController } from "../controllers/EssayController";
+import { isSubscribed } from "../middleware/SubscribedMiddleware";
+import ConvertAPI from "convertapi";
+import fs from "fs/promises";
+import path from "path";
+import os from "os";
+import { v4 as uuidv4 } from "uuid"
 
 const essayRouter = Router()
 
@@ -27,9 +33,55 @@ const findByUuid = async (req: Request, res: Response): Promise<any> => {
     }
 }
 
+const convertToPdf = async (req: Request, res: Response): Promise<any> => {
+    try {
+        const { essay, title }: { essay: string, title: string } = req.body
+        const convertApi = new ConvertAPI(process.env.CONVERTAPI_KEY!)
+
+        const htmlContent = `
+        <html>
+            <head>
+            <style>
+                body { font-family: 'Arial', sans-serif; font-size: 28px; }
+            </style>
+            </head>
+            <body>
+            ${essay.replace(/\n/g, "<br>")}
+            </body>
+        </html>
+        `;
+        const tempDir = os.tmpdir()
+        const tempFilePath = path.join(tempDir, `${title.replace(/ /g, '-')}-${uuidv4()}.html`)
+        await fs.writeFile(tempFilePath, htmlContent, "utf-8")
+
+        const result = await convertApi.convert('pdf', {
+            File: tempFilePath
+        }, 'html')
+
+        await fs.unlink(tempFilePath)
+
+        const pdfUrl = result.files[0].url
+        return res.status(200).json({
+            success: true,
+            message: pdfUrl
+        })
+    } catch (error) {
+        console.log(error)
+        return res.status(500).json({
+            success: false,
+            message: "Internal server error"
+        })
+    }
+}
+
 essayRouter.post(
     '/essay/find-by-uuid',
     findByUuid
+)
+essayRouter.post(
+    '/essay/convert-to-pdf',
+    //isSubscribed,
+    convertToPdf
 )
 
 export default essayRouter
