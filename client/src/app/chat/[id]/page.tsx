daf6789cdd65f2bf24bf1a4c8d726e076b21539b
IamMax279 <maksssiula@gmail.com>
2025-07-31 15:00:03 +0200

    add convertToPdf function

--- client/src/app/chat/[id]/page.tsx
+++ client/src/app/chat/[id]/page.tsx
@@ -2,16 +2,22 @@
 
 import { useMutation } from "@tanstack/react-query"
 import { useParams, useSearchParams, useRouter } from "next/navigation"
-import { useEffect, useState } from "react"
+import { useEffect, useRef, useState } from "react"
 import axios from "axios"
 import { useTypewriter } from "@/hooks/useTypewriter"
 import Logo from "../../../../public/LogoWhite.svg"
 import Image from "next/image"
+import html2canvas from "html2canvas"
+import { jsPDF } from "jspdf"
+import SmallButton from "@/components/SmallButton"
 
 export default function Generated() {
     const [essay, setEssay] = useState<string | null>(null)
+    const [essayTitle, setEssayTitle] = useState<string>("")
     const [typewriterDone, setTypewriterDone] = useState<boolean>(false)
     const [typewriterAllowed, setTypewriterAllowed] = useState<boolean>(false)
+
+    const printRef = useRef(null)
     //search params są po "?" w url
     //params są bezpośrednio w url, przed "?"
     const params = useParams()
@@ -24,18 +30,40 @@ export default function Generated() {
 
     const typewriter = useTypewriter(essay ?? "", 50)
 
-    const { mutate: getEssay, isPending } = useMutation({
+    const { mutate: getEssay } = useMutation({
         mutationFn: async (id: string) => {
-            return await axios.post(`${process.env.NEXT_PUBLIC_SERVER_URL}/user/get-essay`,
+            const response = await axios.post(`${process.env.NEXT_PUBLIC_SERVER_URL}/user/get-essay`,
                 { essayId: id },
                 { withCredentials: true }
             )
+
+            return response.data
         },
         onSuccess: (data) => {
-            setEssay(data.data.essay)
+            setEssay(data.essay)
+            setEssayTitle(data.title)
+            console.log(data)
         },
         onError: (error) => {
             router.replace('/chat/nowy')
+        }
+    })
+
+    const { mutate: convertToPdf } = useMutation({
+        mutationFn: async () => {
+            const response = await axios.post(
+                `${process.env.NEXT_PUBLIC_SERVER_URL}/essay/convert-to-pdf`,
+                { essay, title: essayTitle },
+                { withCredentials: true }
+            )
+
+            return response.data
+        },
+        onSuccess: (data) => {
+            window.open(data.message, "_blank")
+        },
+        onError: (error) => {
+            console.log("ERRRORORO:", error)
         }
     })
 
@@ -79,17 +107,27 @@ export default function Generated() {
                 </div>
             </div>
             :
-            <div className='relative flex flex-row mx-auto lg:w-[656px] md:w-[420px] w-[70%] min-w-[220px]
-                border border-bigbutton/70 rounded-lg bg-[#141414] my-12'>
-                <Image
-                src={Logo}
-                alt='logo'
-                className={`hidden md:block md:absolute md:top-2 md:-left-[54px] md:w-10 md:h-10`}
-                />
-                <div className="font-mono text-base leading-relaxed p-6 w-full min-h-12 
-                whitespace-pre-wrap break-words text-gray-200">
-                    <span className="tracking-wide font-outfit">{essay}</span>
+            <div className="flex flex-col items-center">
+                <div className='relative flex flex-row mx-auto lg:w-[656px] md:w-[420px] w-[70%] min-w-[220px]
+                    border border-bigbutton/70 rounded-lg bg-[#141414] my-12'>
+                    <Image
+                    src={Logo}
+                    alt='logo'
+                    className={`hidden md:block md:absolute md:top-2 md:-left-[54px] md:w-10 md:h-10`}
+                    />
+                    <div className="font-mono text-base leading-relaxed p-6 w-full min-h-12 
+                    whitespace-pre-wrap break-words text-gray-200">
+                        <span className="tracking-wide font-outfit"
+                        ref={printRef}>
+                            {essay}
+                        </span>
+                    </div>
                 </div>
+                <SmallButton
+                onPress={() => convertToPdf()}
+                text="Pobierz PDF"
+                className="text-white -mt-6 mb-8"
+                />
             </div>
             }
         </div>
