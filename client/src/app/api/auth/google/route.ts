74bf10b6fb5772bb93b85324e294c5e130603273
IamMax279 <maksssiula@gmail.com>
2025-08-02 20:48:46 +0200

    use the localhost version of server url in the google auth api route

--- client/src/app/api/auth/google/route.ts
+++ client/src/app/api/auth/google/route.ts
@@ -1,5 +1,5 @@
 import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(req: NextRequest) {
-    return NextResponse.redirect(process.env.SERVER_URL! + "/auth/google")
+    return NextResponse.redirect(process.env.NEXT_PUBLIC_SERVER_URL! + "/auth/google")
 }
